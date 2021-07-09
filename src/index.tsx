051c41378cff912d64a21e3c0aea2e25e9aceda8
Jason Kurian <jason.kurian@shopify.com>
2021-07-09 15:54:26 -0400

    fix lint command

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
+import { ReactElement } from "react";
 import styles from "./styles.module.css";
 
 interface Props {
   text: string;
 }
 
-export const ExampleComponent = ({ text }: Props) => {
+export const ExampleComponent = ({ text }: Props): ReactElement => {
   return <div className={styles.test}>Example Component: {text}</div>;
 };
